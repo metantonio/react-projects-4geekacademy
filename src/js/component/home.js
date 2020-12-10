8b93e086bd735896b4f8748b0e6b1c23439ab16e
Antonio Martínez <antonio_martinez88@hotmail.com>
2020-12-10 09:05:31 -0400

    agregada función evaluar ganador por fila

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -18,10 +18,50 @@ export function Home() {
 	]);
 
 	const [board, setBoard] = useState([
+		//por ahora se podría cambiar manualmente los valores para comprobar
 		["x", "o", null],
-		["x", "x", "o"],
+		["x", "o", "x"],
 		["o", null, "o"]
 	]);
+
+	const evaluarPartida = boardHere => {
+		let winner = "";
+		//a continuación se proponen las coordenadas ganaradoras en la matriz 3x3
+		/*const lines = [
+			[00, 01, 02], //esta serían las coordenadas de la fila 1 (están en número, tal vez me convenga texto ya que podría dar error el octal 00)
+			[10, 11, 12],
+			[20, 21, 22],
+			[00, 10, 20], //estas serían las coordenadas de la columna 1
+			[01, 11, 21],
+			[02, 12, 22],
+			[00, 11, 22], //esta sería una diagonal
+			[02, 11, 20] //esta sería la otra diagonal
+		];*/
+		/*viendo las posiciones posibles están los casos ganadores cuando:
+		 -fila son iguales: i0=i1=i2
+		 -columnas iguales: 0j=1j=2j
+		 -diagonales: i=j ó 02=11=20 */
+
+		//Aquí se empezará a recorrer las filas de la matriz para evaluar filas iguales
+
+		/*boardHere.map((row, indexRow) =>{ //esta forma de tratar de integrar el board es incorrecta
+			if (row[0] && row[0] == row[1] && row[1] == row[2]) {
+				winner = row[0];
+				//break;
+			}
+		}*/
+
+		//como no funcionó la función map, se intentará con loop for
+		//nota: boardHere es una variable interna declarada más arriba para meter el Estado board
+		for (let row of boardHere) {
+			if (row[0] && row[0] == row[1] && row[1] == row[2]) {
+				winner = row[0];
+				alert("victoria por filas iguales");
+				break;
+			}
+		}
+	};
+
 	return (
 		<div className="container-fluid">
 			<div className="text-center row">
@@ -68,6 +108,7 @@ export function Home() {
 								</React.Fragment>
 							);
 						})}
+						;{evaluarPartida(board)};
 					</div>
 				)}
 			</div>
