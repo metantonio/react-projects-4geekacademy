80109efede65762e2701a2e22f03763a178caeef
Antonio Martínez <antonio_martinez88@hotmail.com>
2020-12-14 18:10:04 -0400

    agregado click en tablero

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 //create your first component
 export function Home() {
@@ -19,15 +19,16 @@ export function Home() {
 
 	const [board, setBoard] = useState([
 		//por ahora se podría cambiar manualmente los valores para comprobar
-		["x", "o", "o"],
-		["o", "x", "x"],
-		["o", "x", "x"]
+		[null, null, null],
+		[null, null, null],
+		[null, null, null]
 	]);
 
-	const evaluarPartida = boardHere => {
-		let winner = "";
-		//a continuación se proponen las coordenadas ganadoras en la matriz 3x3
-		/*const lines = [
+	useEffect(() => {
+		const evaluarPartida = boardHere => {
+			let winner = "";
+			//a continuación se proponen las coordenadas ganadoras en la matriz 3x3
+			/*const lines = [
 			[00, 01, 02], //esta serían las coordenadas de la fila 1 (están en número, tal vez me convenga texto ya que podría dar error el octal 00)
 			[10, 11, 12], //fila 2
 			[20, 21, 22], //fila 3
@@ -37,66 +38,97 @@ export function Home() {
 			[00, 11, 22], //esta sería una diagonal
 			[02, 11, 20] //esta sería la otra diagonal
 		];*/
-		/*viendo las posiciones posibles están los casos ganadores cuando:
+			/*viendo las posiciones posibles están los casos ganadores cuando:
 		 -fila son iguales: i0=i1=i2
 		 -columnas iguales: 0j=1j=2j
 		 -diagonales: i=j ó 02=11=20 */
 
-		//Aquí se empezará a recorrer las filas de la matriz para evaluar filas iguales
+			//Aquí se empezará a recorrer las filas de la matriz para evaluar filas iguales
 
-		/*boardHere.map((row, indexRow) =>{ //esta forma de tratar de integrar el board es incorrecta y row es un número no un arreglo
+			/*boardHere.map((row, indexRow) =>{ //esta forma de tratar de integrar el board es incorrecta y row es un número no un arreglo
 			if (row[0] && row[0] == row[1] && row[1] == row[2]) {
 				winner = row[0];
 				//break;
 			}
 		}*/
 
-		//como no funcionó la función map, se intentará con loop for
-		let columns = [[], [], []]; //se sabe que esta es la estructura de las coordenadas ganadoras a buscar
-		//nota: boardHere es una variable interna declarada más arriba para meter el Estado board
-		for (let row of boardHere) {
-			if (row[0] && row[0] == row[1] && row[1] == row[2]) {
-				winner = row[0];
-				alert("victoria por filas iguales, símbolo ganador: " + winner);
-				break;
-			}
-			//se intenta tener ahora la misma estructura de matriz que las coordenadas
-			columns[0].push(row[0]);
-			columns[1].push(row[1]);
-			columns[2].push(row[2]);
-		}
-
-		//ahora con la estructura de coordenadas ganadoras creada, se puede evaluar si hay
-		//una columna ganadora al tener la misma pinta, en caso de no haber ganador por fila.
-		if (winner == "") {
-			for (let col of columns) {
-				if (col[0] && col[0] == col[1] && col[1] == col[2]) {
-					winner = col[0];
+			//como no funcionó la función map, se intentará con loop for
+			let columns = [[], [], []]; //se sabe que esta es la estructura de las coordenadas ganadoras a buscar
+			//nota: boardHere es una variable interna declarada más arriba para meter el Estado board
+			for (let row of boardHere) {
+				if (row[0] && row[0] == row[1] && row[1] == row[2]) {
+					winner = row[0];
 					alert(
-						"victoria por columnas iguales, símbolo ganador: " +
-							winner
+						"victoria por filas iguales, símbolo ganador: " + winner
 					);
 					break;
 				}
+				//se intenta tener ahora la misma estructura de matriz que las coordenadas
+				columns[0].push(row[0]);
+				columns[1].push(row[1]);
+				columns[2].push(row[2]);
 			}
 
-			//ahora se evalúan las diagonales
+			//ahora con la estructura de coordenadas ganadoras creada, se puede evaluar si hay
+			//una columna ganadora al tener la misma pinta, en caso de no haber ganador por fila.
 			if (winner == "") {
-				if (
-					(boardHere[1][1] &&
-						boardHere[0][0] == boardHere[1][1] &&
-						boardHere[1][1] == boardHere[2][2]) ||
-					(boardHere[1][1] &&
-						boardHere[2][0] == boardHere[1][1] &&
-						boardHere[1][1] == boardHere[0][2])
-				) {
-					winner = boardHere[1][1];
-					alert("victoria por diagonal, símbolo ganador: " + winner);
+				for (let col of columns) {
+					if (col[0] && col[0] == col[1] && col[1] == col[2]) {
+						winner = col[0];
+						alert(
+							"victoria por columnas iguales, símbolo ganador: " +
+								winner
+						);
+						break;
+					}
+				}
+
+				//ahora se evalúan las diagonales
+				if (winner == "") {
+					if (
+						(boardHere[1][1] &&
+							boardHere[0][0] == boardHere[1][1] &&
+							boardHere[1][1] == boardHere[2][2]) ||
+						(boardHere[1][1] &&
+							boardHere[2][0] == boardHere[1][1] &&
+							boardHere[1][1] == boardHere[0][2])
+					) {
+						winner = boardHere[1][1];
+						alert(
+							"victoria por diagonal, símbolo ganador: " + winner
+						);
+					}
 				}
 			}
-		}
-		return winner; //pedimos el Estado de regreso para luego pasarlo por setStatusPlay
+			return winner; //pedimos el Estado de regreso para luego pasarlo por setStatusPlay
+		};
+	}, []);
+
+	const [turn, setTurn] = useState(null);
+
+	//se hace esta función para saber en qué celda fue hecho el click
+	const TurnGame = (indexRow, indexCol) => {
+		let newboard = board.map((row, rowIndex) => {
+			return row.map((col, colIndex) => {
+				return rowIndex == indexRow && colIndex == indexCol
+					? turn
+					: col;
+			});
+		});
+		setBoard(newboard);
 	};
+
+	useEffect(() => {
+		if (turn == null) {
+			setTurn("x");
+		} else {
+			if (turn == "x") {
+				setTurn("o");
+			} else {
+				setTurn("x");
+			}
+		}
+	});
 
 	return (
 		<div className="container-fluid">
@@ -129,6 +161,12 @@ export function Home() {
 									{row.map((cell, indexCol) => {
 										return (
 											<div
+												onClick={e => {
+													TurnGame(
+														indexRow,
+														indexCol
+													);
+												}}
 												key={`${indexRow}-${indexCol}`}
 												className="cell d-flex justify-content-center align-items-center">
 												<span className="d-flex">
@@ -144,10 +182,11 @@ export function Home() {
 								</React.Fragment>
 							);
 						})}
-						;{evaluarPartida(board)};
+						;
 					</div>
 				)}
 			</div>
+
 			<div className="row justify-content-center">
 				<button
 					className="btn btn-primary"
