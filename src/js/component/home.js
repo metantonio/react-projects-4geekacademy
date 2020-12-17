336d2ea2222fd645f6d3115fe38056e487d22c62
Antonio Martínez <antonio_martinez88@hotmail.com>
2020-12-17 12:55:23 -0400

    crea usuario al recargar página

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,7 +5,7 @@ import "bootstrap";
 
 //Para este proyecto de To Do List with Fetch, primero hay que crear nuestro usuario en la API de BreathCode
 //API:  https://assets.breatheco.de/apis/fake/todos/
-const BASE_URL = "https://assets.breatheco.de/apis/fake/todos/";
+const BASE_URL = "https://assets.breatheco.de/apis/fake/todos";
 //Haciendo POST https://assets.breatheco.de/apis/fake/todos/user/metantonio
 //dará error porque el servidor pide tener un arreglo vacío en el body (content .JSON), agregarlo. Debe arrojar Status: 200
 
@@ -17,6 +17,7 @@ const BASE_URL = "https://assets.breatheco.de/apis/fake/todos/";
 const Home = () => {
 	const [tarea, guardarTarea] = useState("");
 	const [lista, guardarLista] = useState([]);
+
 	const deleteItems = indexItem => {
 		guardarLista(prevState =>
 			prevState.filter((todo, index) => index !== indexItem)
@@ -24,29 +25,111 @@ const Home = () => {
 	};
 
 	const eliminaTarea = indexItem => {
+		let nuevaLista = [];
 		guardarLista(prevState =>
 			prevState.filter((todo, index) => index !== indexItem)
 		);
+		{
+			if (nuevaLista.length > 1) {
+				let response = fetch(`${BASE_URL}/user/metantonio`, {
+					method: "PUT",
+					body: JSON.stringify(nuevaLista),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				});
+			} else {
+				let response = fetch(`${BASE_URL}/user/metantonio`, {
+					method: "DELETE",
+					body: JSON.stringify(nuevaLista),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				});
+			}
+		}
+		return nuevaLista;
+	};
+
+	const crearUsuario = () => {
+		let url = `${BASE_URL}/user/metantonio`;
+		//por defecto fetch es método GET y .json, el cuerpo del json (la respuesta) es pasada a la variable APILista y se guarda
+		fetch(url)
+			.then(response => response.json())
+			.then(APILista => guardarLista(APILista))
+			.catch(error => {
+				console.log("está ocurriendo un error");
+				{
+					let response = fetch(`${BASE_URL}/user/metantonio`, {
+						method: "POST",
+						body: JSON.stringify([
+							{
+								label: "sample task",
+								done: false
+							}
+						]),
+						headers: {
+							"Content-Type": "application/json"
+						}
+					});
+				}
+				fetch(url)
+					.then(response => response.json())
+					.then(APILista => guardarLista(APILista));
+			});
 	};
 
 	useEffect(() => {
 		let url = `${BASE_URL}/user/metantonio`;
 		//por defecto fetch es método GET y .json, el cuerpo del json (la respuesta) es pasada a la variable APILista y se guarda
-		// fetch(url)
-		// 	.then(response => response.json())
-		// 	.then(APILista => guardarLista(APILista))
-		// 	.catch(error => console.log(`{error}`));
+		fetch(url)
+			.then(response => response.json())
+			.then(APILista => guardarLista(APILista))
+			.catch(error => {
+				console.log("está ocurriendo un error");
+				{
+					let response = fetch(`${BASE_URL}/user/metantonio`, {
+						method: "POST",
+						body: JSON.stringify([
+							{
+								label: "sample task",
+								done: false
+							}
+						]),
+						headers: {
+							"Content-Type": "application/json"
+						}
+					});
+				}
+				fetch(url)
+					.then(response => response.json())
+					.then(APILista => guardarLista(APILista));
+			});
 
 		//otra forma para no tener .then().then()... de forma que sea más legible
-		const getTask = async () => {
-			//esto obliga a javascript a esperar por la promesa
-			let response = await fetch(url);
-			//ahora response en un valor y no una promesa hasta no tener
-			//respuesta del servidor así que habría que esperar a tener respuesta para que response sea una promesa
-			let APILista = await response.json();
-			guardarLista(APILista);
-		};
-		getTask(url);
+		// const getTask = async () => {
+		// 	//esto obliga a javascript a esperar por la promesa
+		// 	let response = await fetch(url);
+		// 	//ahora response en un valor y no una promesa hasta no tener
+		// 	//respuesta del servidor así que habría que esperar a tener respuesta para que response sea una promesa
+		// 	let APILista = await response.json();
+		// 	guardarLista(APILista);
+		// 	if (fetch(url).catch()) {
+		// let response = fetch(`${BASE_URL}/user/metantonio`, {
+		// 	method: "POST",
+		// 	body: JSON.stringify([
+		// 		{
+		// 			label: "sample task",
+		// 			done: false
+		// 		}
+		// 	]),
+		// 	headers: {
+		// 		"Content-Type": "application/json"
+		// 	}
+		// });
+		// 	}
+		// };
+		// getTask(url);
 	}, []);
 
 	return (
@@ -68,10 +151,11 @@ const Home = () => {
 									label: tarea,
 									done: false
 								});
+								crearUsuario();
 								let response = await fetch(
 									`${BASE_URL}/user/metantonio`,
 									{
-										method: "POST",
+										method: "PUT",
 										body: JSON.stringify(mostrarLista),
 										headers: {
 											"Content-Type": "application/json"
@@ -81,6 +165,8 @@ const Home = () => {
 								if (response.ok) {
 									guardarLista(mostrarLista);
 									guardarTarea((e.target.value = ""));
+								} else {
+									console.log("error cargando");
 								}
 							}
 						}}
@@ -99,8 +185,40 @@ const Home = () => {
 										{tareas.label}
 										<button
 											className="btn btn-light"
-											onClick={e => {
-												deleteItems(index);
+											onClick={async e => {
+												// {
+												// 	if (lista.length > 1) {
+												// 		let response = await fetch(
+												// 			`${BASE_URL}/user/metantonio`,
+												// 			{
+												// 				method: "PUT",
+												// 				body: JSON.stringify(
+												// 					lista
+												// 				),
+												// 				headers: {
+												// 					"Content-Type":
+												// 						"application/json"
+												// 				}
+												// 			}
+												// 		);
+												// 	} else {
+												// 		let response = fetch(
+												// 			`${BASE_URL}/user/metantonio`,
+												// 			{
+												// 				method:
+												// 					"DELETE",
+												// 				body: JSON.stringify(
+												// 					lista
+												// 				),
+												// 				headers: {
+												// 					"Content-Type":
+												// 						"application/json"
+												// 				}
+												// 			}
+												// 		);
+												// 	}
+												// }
+												eliminaTarea(index);
 											}}>
 											<i
 												className="fa fa-times"
@@ -115,7 +233,7 @@ const Home = () => {
 					<p className="card-footer text-muted">
 						{lista.length > 0
 							? `${lista.length} tareas por completar`
-							: "Agregar tareas"}
+							: "Sin tareas por hacer: Usuario Borrado. Recarga la página para crearlo"}
 					</p>
 				</div>
 			</div>
